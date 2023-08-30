dd9c7e1040ae845595605253988c8c3a3cad8a68
Martin Visser <martin.visser01@rabobank.nl>
2023-08-30 16:35:53 +0200

    Add keys to migrate to plugin specific config

--- git-plugin/src/main/kotlin/io/rabobank/ret/git/plugin/GitEntryCommand.kt
+++ git-plugin/src/main/kotlin/io/rabobank/ret/git/plugin/GitEntryCommand.kt
@@ -3,11 +3,13 @@ package io.rabobank.ret.git.plugin
 import io.quarkus.picocli.runtime.annotations.TopCommand
 import io.quarkus.runtime.annotations.RegisterForReflection
 import io.rabobank.ret.RetContext
+import io.rabobank.ret.commands.PluginConfigureCommand
 import io.rabobank.ret.commands.PluginInitializeCommand
 import io.rabobank.ret.git.plugin.command.AutoCompleteCommand
 import io.rabobank.ret.git.plugin.command.PipelineCommand
 import io.rabobank.ret.git.plugin.command.PullRequestCommand
 import io.rabobank.ret.git.plugin.command.RepositoryCommand
+import io.rabobank.ret.util.Logged
 import picocli.CommandLine.Command
 
 @TopCommand
@@ -16,6 +18,7 @@ import picocli.CommandLine.Command
     description = ["Plugin to interact with git provider"],
     subcommands = [
         PluginInitializeCommand::class,
+        PluginConfigureCommand::class,
         PullRequestCommand::class,
         PipelineCommand::class,
         RepositoryCommand::class,
@@ -23,4 +26,5 @@ import picocli.CommandLine.Command
     ],
 )
 @RegisterForReflection(targets = [RetContext::class])
+@Logged
 class GitEntryCommand
--- git-plugin/src/main/kotlin/io/rabobank/ret/git/plugin/provider/azure/AzureDevopsPluginConfig.kt
+++ git-plugin/src/main/kotlin/io/rabobank/ret/git/plugin/provider/azure/AzureDevopsPluginConfig.kt
@@ -18,6 +18,17 @@ class AzureDevopsPluginConfig : BasePluginConfig() {
         ConfigurationProperty(ORGANIZATION, "Enter your Azure organization", required = true),
     )
 
+    /**
+     * This will copy configuration from ret.config into plugins/git.json
+     */
+    override fun keysToMigrate(): List<Pair<String, String>> =
+        listOf(
+            EMAIL to EMAIL,
+            PAT to PAT,
+            PROJECT to PROJECT,
+            ORGANIZATION to ORGANIZATION,
+        )
+
     private companion object {
         private const val EMAIL = "azure_devops_email"
         private const val PAT = "azure_devops_pat"
