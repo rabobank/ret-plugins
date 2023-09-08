e97d15c28086d71876f7f0e8ae56352c934fe4af
Martin Visser <martin.visser01@rabobank.nl>
2023-09-08 13:14:11 +0200

    Remove git- prefix for zsh autocomplete

--- git-plugin/src/main/resources/autocompletion/zsh/completions.zsh
+++ git-plugin/src/main/resources/autocompletion/zsh/completions.zsh
@@ -3,7 +3,7 @@ function _autocomplete_branch() {
     if _matches_flag_syntax "$word"; then return; fi
     _contains_any_flag '-ica' '--ignore-context-aware' && local ignore_context_aware_flag='-ica'
     local repository_flag_value=${RET_COMBINED_OPT_ARGS[-r]-${RET_COMBINED_OPT_ARGS[--repository]}}
-    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete git-branch --word="$word" --repository="${repository_flag_value}" ${ignore_context_aware_flag})}")
+    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete branch --word="$word" --repository="${repository_flag_value}" ${ignore_context_aware_flag})}")
     vals=( ${desc%%:*} )
     compadd -d desc -aQU vals
     compstate[insert]=menu # no expand
@@ -12,7 +12,7 @@ function _autocomplete_branch() {
 function _autocomplete_repository() {
     local word=${words[$CURRENT]}
     if _matches_flag_syntax "$word"; then return; fi
-    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete git-repository --word="$word")}")
+    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete repository --word="$word")}")
     vals=( ${desc%%:*} )
     compadd -d desc -aQU vals
     compstate[insert]=menu # no expand
@@ -26,7 +26,7 @@ function _autocomplete_repository_flag() {
     local flag_word_prefix=$([[ $word =~ ^.*= ]] && echo -n "$MATCH")
     # Get the value from the option -r or else --repository
     local repository_flag_value=$flag_value
-    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete git-repository --word="${repository_flag_value}")}")
+    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete repository --word="${repository_flag_value}")}")
     vals=( ${desc%%:*} )
     compadd -p "$flag_word_prefix" -d desc -aQU vals
     compstate[insert]=menu # no expand
@@ -35,7 +35,7 @@ function _autocomplete_repository_flag() {
 function _autocomplete_pipeline() {
     local word=${(Q)words[$CURRENT]} # (Q) unescapes things like \(, \) etc
     if _matches_flag_syntax "$word"; then return; fi
-    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete git-pipeline --word="${word}")}")
+    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete pipeline --word="${word}")}")
     vals=( ${desc%%:*} )
     compadd -V "no-sort-group" -d desc -aU vals # not sure why "-o nosort" does not work and why -V is...
     compstate[list_max]="300"
@@ -46,7 +46,7 @@ function _autocomplete_pipeline_run() {
     local word=${words[$CURRENT]}
     local pipeline_id=${(Q)words[$CURRENT-1]} # (Q) unescapes things like \(, \) etc
     if _matches_flag_syntax "$word"; then return; fi
-    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete git-pipeline-run --pipeline-id="${pipeline_id}" --word="$word")}")
+    desc=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete pipeline-run --pipeline-id="${pipeline_id}" --word="$word")}")
     vals=( ${desc%%:*} )
     compadd -V "no-sort-group" -d desc -aQU vals # not sure why "-o nosort" does not work and why -V is...
     compstate[insert]=menu # no expand
@@ -57,10 +57,9 @@ function _autocomplete_pullrequest() {
     if _matches_flag_syntax "$word"; then return; fi
     _contains_any_flag '-ica' '--ignore-context-aware' && local ignore_context_aware_flag='-ica'
     local repository_flag_value=${RET_COMBINED_OPT_ARGS[-r]-${RET_COMBINED_OPT_ARGS[--repository]}}
-    candidates=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete git-pullrequest --word="$word" --repository="${repository_flag_value}" ${ignore_context_aware_flag})}")
+    candidates=("${(@f)$(RET_ENV=ZSH_AUTOCOMPLETE ret git autocomplete pullrequest --word="$word" --repository="${repository_flag_value}" ${ignore_context_aware_flag})}")
     desc=( ${candidates#*:} )
     vals=( ${candidates%%:*} )
     compadd -d desc -aQU vals
     compstate[insert]=menu # no expand
 }
-
