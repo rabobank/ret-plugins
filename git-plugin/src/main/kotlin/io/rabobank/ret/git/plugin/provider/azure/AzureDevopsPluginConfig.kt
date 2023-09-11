ac734d95c389235a380c4b59efe9dfd5e70f2dc1
Martin Visser <martin.visser01@rabobank.nl>
2023-09-11 09:09:58 +0200

    Key are named the same in AzDo

--- git-plugin/src/main/kotlin/io/rabobank/ret/git/plugin/provider/azure/AzureDevopsPluginConfig.kt
+++ git-plugin/src/main/kotlin/io/rabobank/ret/git/plugin/provider/azure/AzureDevopsPluginConfig.kt
@@ -16,17 +16,6 @@ class AzureDevopsPluginConfig : Configurable() {
         ConfigurationProperty(ORGANIZATION, "Enter your Azure organization", required = true),
     )
 
-    /**
-     * This will copy configuration from ret.config into plugins/git.json
-     */
-    override fun keysToMigrate(): List<Pair<String, String>> =
-        listOf(
-            EMAIL to EMAIL,
-            PAT to PAT,
-            PROJECT to PROJECT,
-            ORGANIZATION to ORGANIZATION,
-        )
-
     private companion object {
         private const val EMAIL = "azure_devops_email"
         private const val PAT = "azure_devops_pat"
